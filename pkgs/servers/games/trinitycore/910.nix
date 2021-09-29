d148162c5c89baf19a78550bd6cf48bb44e1c83a
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-09-29 14:08:51 +0200

    trinitycore_910: fix hash

--- pkgs/servers/games/trinitycore/910.nix
+++ pkgs/servers/games/trinitycore/910.nix
@@ -3,5 +3,5 @@
 callPackage ./generic.nix (args // {
   version = "TDB910.21081";
   commit = "b9ffac3afa485bff66265a3703604d15c78228d1";
-  sha256 = "1h06a0cm3z6jn41bvsa61lr1nj0zk3q1gh04k1bd4s5caykpfp2n";
+  sha256 = "0gmhvf2nacahpjpl83prad6p2pklhsjfdb7rsk939l4jpv5dcbhb";
 })
