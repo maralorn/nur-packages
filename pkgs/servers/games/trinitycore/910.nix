6cfab989faf0a916ada577081b5a1717a06c28e0
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-09-29 13:14:18 +0200

    trinitycore: leaveDotGit

--- pkgs/servers/games/trinitycore/910.nix
+++ pkgs/servers/games/trinitycore/910.nix
@@ -3,5 +3,5 @@
 callPackage ./generic.nix (args // {
   version = "TDB910.21081";
   commit = "b9ffac3afa485bff66265a3703604d15c78228d1";
-  sha256 = "02c866zpr9ra2pw0ks7lxgx8j3lbka9nxy8f6ll85zz7ryajlz5j";
+  sha256 = "1h06a0cm3z6jn41bvsa61lr1nj0zk3q1gh04k1bd4s5caykpfp2n";
 })
